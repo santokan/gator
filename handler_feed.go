bb1dc3c9cf9c22bb9142c3029f2856526b5fe332
santokan <houska.peter@gmail.com>
2025-03-30 23:51:16 +0200

    refactor: moved follow feeds to it's own file

--- handler_feed.go
+++ handler_feed.go
@@ -75,66 +75,6 @@ func handlerFeeds(s *state, c command) error {
 	return nil
 }
 
-func handlerFollow(s *state, c command) error {
-	if len(c.Args) != 1 {
-		return fmt.Errorf("usage: follow <feed_url>")
-	}
-
-	feedURL := c.Args[0]
-	feed, err := s.db.GetFeedByURL(context.Background(), feedURL)
-	if err != nil {
-		return fmt.Errorf("unable to find feed with URL %s: %v", feedURL, err)
-	}
-
-	userID, err := s.db.GetUserIDbyUsername(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("unable to get userID for user '%s': %v", s.cfg.CurrentUserName, err)
-	}
-
-	followID := uuid.New()
-	now := time.Now()
-
-	_, err = s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
-		ID:        followID,
-		CreatedAt: now,
-		UpdatedAt: now,
-		UserID:    userID,
-		FeedID:    feed.ID,
-	})
-	if err != nil {
-		return fmt.Errorf("unable to create feed follow: %v", err)
-	}
-
-	fmt.Printf("Following feed '%s' for user '%s'\n", feed.Name, s.cfg.CurrentUserName)
-	return nil
-}
-
-func handlerFollowing(s *state, c command) error {
-	if len(c.Args) != 0 {
-		return fmt.Errorf("usage: following")
-	}
-
-	userID, err := s.db.GetUserIDbyUsername(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("unable to get userID for user '%s': %v", s.cfg.CurrentUserName, err)
-	}
-
-	follows, err := s.db.GetFeedFollowsForUser(context.Background(), userID)
-	if err != nil {
-		return fmt.Errorf("unable to get feed follows for user '%s': %v", s.cfg.CurrentUserName, err)
-	}
-
-	fmt.Println("List of feeds followed by the user:")
-	for _, follow := range follows {
-		fmt.Printf("* Feed name:        %s\n", follow.FeedName)
-		fmt.Printf("* Feed URL:         %s\n", follow.FeedID)
-		fmt.Printf("* Username:         %s\n", follow.UserName)
-		fmt.Println()
-	}
-
-	return nil
-}
-
 func printFeed(feed database.Feed) {
 	fmt.Printf("* ID:            %s\n", feed.ID)
 	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
